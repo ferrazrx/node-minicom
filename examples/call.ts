13441478c3d2ff1852e7778ec0d28da81bd0bd31
Rafael Ferraz <fer16002@byui.edu>
2023-06-13 18:20:24 -0300

    Add CPCMREG

--- examples/call.ts
+++ examples/call.ts
@@ -10,7 +10,9 @@ const port = minicom.addPort({
 
 port.callPhoneNumber({
   onData: (data)=> {
+    console.log(data)
     if(data.code === 'VOICE CALL: BEGIN'){
+      console.log("TRYING TO PLAY...")
       port.audioMessage()
     }
   },
